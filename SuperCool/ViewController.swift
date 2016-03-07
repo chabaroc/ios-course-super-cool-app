97eb494f61e5884a08f10a372f787c6aa02c2ee4
Camille Chabaro <Camille@Camilles-MacBook-Pro.local>
2016-03-07 22:21:28 +0700

    added files for supercool

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Camille Chabaro on 3/7/16.
+//  Copyright © 2016 Camille Chabaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bg: UIImageView!
+    @IBOutlet weak var boomApp: UIImageView!
+    @IBOutlet weak var lebuttan: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func pressZeButtan(sender: AnyObject) {
+        
+        bg.hidden = false
+        boomApp.hidden = false
+        lebuttan.hidden = true
+    }
+
+
+}
+
